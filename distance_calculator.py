f3274eea74404d918f4ffc655b2e38dbd0e0c701
Mukesh Odeti <139321486+omukesh@users.noreply.github.com>
2024-12-27 14:42:17 +0530

    Update distance_calculator.py
    # refactored this to only one function to handle any class

--- distance_calculator.py
+++ distance_calculator.py
@@ -5,9 +5,8 @@ import config
 cctv_url = config.CCTV_URL
 
 class DistanceCalculator:
-    def __init__(self, known_width_person, known_width_monitor, focal_length=None):
-        self.known_width_person = known_width_person
-        self.known_width_monitor = known_width_monitor
+    def __init__(self, known_width=0.4, focal_length=None):
+        self.known_width = known_width
         self.focal_length = focal_length or self.calculate_focal_length()
 
     def calculate_focal_length(self, img_width=868, known_distance=1, known_width=0.2):
@@ -15,7 +14,7 @@ class DistanceCalculator:
         ret, frame = cap.read()
         cap.release()
 
-        pixel_width = 200  # Measure this from the frame
+        pixel_width = 200  # Measure this from the frame using a common ruler across the monitor
         self.focal_length = (pixel_width * known_distance) / known_width
         return self.focal_length
 
@@ -24,12 +23,7 @@ class DistanceCalculator:
             raise ValueError("Focal length needs to be calculated")
         return (known_width * self.focal_length) / perceived_width
 
-    def calculate_distance_person(self, box):
+    def calculate_distance_class(self, box):  
         x1, y1, x2, y2 = map(int, box.xyxy[0])
         perceived_width = x2 - x1
-        return self.calculate_distance(perceived_width, self.known_width_person)
-
-    def calculate_distance_monitor(self, box):
-        x1, y1, x2, y2 = map(int, box.xyxy[0])
-        perceived_width = x2 - x1
-        return self.calculate_distance(perceived_width, self.known_width_monitor)
+        return self.calculate_distance(perceived_width)
